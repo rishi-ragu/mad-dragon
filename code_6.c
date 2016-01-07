302faba5d57ce1c1b55d33f8c90711fe852c42e4
rishi-ragu <rishiragunathan@outlook.com>
2016-01-07 08:47:49 -0800

    Create code_6.c

---
+++ code_6.c
@@ -0,0 +1,16 @@
+//Program to find whether the given year is a leap year or not
+#include<stdio.h>
+void main()
+{
+int year;
+printf("Enter a year to check: ");
+scanf("%d",&year);
+if(year%400==0)
+printf("%d is a leap year.",year);
+else if(year%100==0)
+printf("%d is not a leap year.",year);
+else if(year%4==0)
+printf("%d is a leap year.",year);
+else
+printf("%d is not a leap year.");
+}
