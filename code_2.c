b3c176634b8e854d288403b05493ae2fbec86597
rishi-ragu <rishiragunathan@outlook.com>
2016-01-07 08:23:12 -0800

    Create code_2.c

---
+++ code_2.c
@@ -0,0 +1,12 @@
+//Program to find whether a number is odd or even
+#include<stdio.h>
+void main()
+{
+int num;
+printf("Enter a number: ");
+scanf("%d",&num);
+if(num%2==0)
+printf("The given number is even.");
+else
+printf("The given number is odd.");
+}
